8a85d523c810b65301a021f6bfc569df715babf8
王丰圆 <wangfengyuan.123@bytedance.com>
2025-04-07 19:13:51 +0800

    feat: init project

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: [
+      "puppeteer-core",
+    ],
+  },
+  output: "standalone",
 };
 
 export default nextConfig;
